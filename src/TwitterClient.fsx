d3d7c6356032f7de389721dc5e4b878d9eb09a35
shaishav <shaishav.shah.cs@gmail.com>
2020-11-23 19:51:55 -0500

    Add client-server remote communication

--- src/TwitterClient.fsx
+++ src/TwitterClient.fsx
@@ -1,11 +1,61 @@
 namespace TwitterClient
 
 #r "nuget: Akka.FSharp"
-#load @"./TwitterEngine.fsx"
+#r "nuget: Akka.Remote"
 
-open TwitterEngine
+open Akka.Actor
+open Akka.FSharp
+open Akka.Configuration
+
+open System
 
 module TwitterClient = 
+    // Remote configuration
+    let configuration = 
+        ConfigurationFactory.ParseString(
+            @"akka {
+                log-config-on-start : off
+                stdout-loglevel : DEBUG
+                loglevel : ERROR
+                actor {
+                    provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                    debug : {
+                        receive : off
+                        autoreceive : off
+                        lifecycle : off
+                        event-stream : off
+                        unhandled : off
+                    }
+                }
+                remote {
+                    helios.tcp {
+                        port = 8888
+                        hostname = ""192.168.0.193""
+                    }
+                }
+            }")
+
+    let system = ActorSystem.Create ("TwitterClient", configuration)
+
+    let Client (mailbox: Actor<_>) = 
+        let rec loop () = actor {
+            let! message = mailbox.Receive()
+
+            match message with
+                | _ -> 
+                    let serverNode = system.ActorSelection("akka.tcp://TwitterEngine@192.168.0.193:7777/user/Server")
+                    serverNode <! "Hello Server!"
+                    printfn "Received message - %A from Simulator." message
+
+            return! loop()
+
+        }
+        loop ()
+    
+    spawn system "Client" Client
+    
+    let client = system.ActorSelection("akka.tcp://TwitterClient@192.168.0.193:8888/user/Client")
+
     let hello() = 
-        TwitterEngine.hello()
-        printfn "Hello from Twitter Client!"
+        client <! "Hello Client!"
+        Console.ReadLine() |> ignore
--- src/TwitterEngine.fsx
+++ src/TwitterEngine.fsx
@@ -1,7 +1,52 @@
-namespace TwitterEngine
-
 #r "nuget: Akka.FSharp"
+#r "nuget: Akka.Remote"
 
-module TwitterEngine = 
-    let hello() = 
-        printfn "Hello from Twitter Engine!"
+open Akka.Actor
+open Akka.FSharp
+open Akka.Configuration
+
+open System
+
+// Remote configuration
+let configuration = 
+    ConfigurationFactory.ParseString(
+        @"akka {
+            log-config-on-start : off
+            stdout-loglevel : DEBUG
+            loglevel : ERROR
+            actor {
+                provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                debug : {
+                    receive : off
+                    autoreceive : off
+                    lifecycle : off
+                    event-stream : off
+                    unhandled : off
+                }
+            }
+            remote {
+                helios.tcp {
+                    port = 7777
+                    hostname = ""192.168.0.193""
+                }
+            }
+        }")
+
+let system = ActorSystem.Create("TwitterEngine", configuration)
+
+let Server (mailbox: Actor<_>) = 
+    let rec loop() = actor {
+        let! message = mailbox.Receive()
+
+        match message with
+            | _ -> printfn "Received message - %A from Client." message
+            
+        return! loop()
+    }
+    loop()
+
+spawn system "Server" Server
+
+printfn "Server is running!"
+
+Console.ReadLine() |> ignore
